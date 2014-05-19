1a0bc1accc550c6241c31b315dcf58a3e7900df1
Bret Curtis <psi29a@gmail.com>
2014-05-19 17:29:32 +0200

    added more types

--- datadiver/datadiver.cpp
+++ datadiver/datadiver.cpp
@@ -1,39 +1,57 @@
-/*
-	datadriver.cpp
+/**
+ datadriver.cpp
 
-	Datadiver library
+ Datadiver library
 
-    Copyright (C) Bret Curtis 2014
+ Copyright (C) Bret Curtis 2014
 
-    This file is part of Datadiver.cpp
+ This file is part of Datadiver.cpp
 
-    Datadiver is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+ Datadiver is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-    Datadiver is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+ Datadiver is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "datadiver.h"
+#include <cstddef>
+
+typedef struct _dd_input {
+	uint8_t is_file;
+	const char *file_path;
+	uint8_t *memory_location;
+} _dd_input;
+
+typedef struct _dd_data {
+	uint32_t *location;
+	uint32_t *type;
+	uint32_t files_found;
+} _dd_data;
+
+typedef struct _dd_output {
+	const char *path;
+} _dd_output;
 
 typedef struct _dd_context {
-	const char *input_path;
-	const char *output_path;
-	uint32_t *file_location;
-	uint32_t *file_type;
-	uint32_t files_found;
+	_dd_input input;
+	_dd_data data;
+	_dd_output output;
 } _dd_context;
 
+DD_SYMBOL DataDiver DataDiver_CreateContext(void) {
+	DataDiver handle = new _dd_context;
 
-DD_SYMBOL dd_context DataDiver_CreateContext (void) {
-	dd_context context = new _dd_context;
-
-	return (context);
+	handle->input.file_path = "";
+	handle->input.is_file = 0;
+	handle->input.memory_location = NULL;
+	handle->data.files_found = 0;
+	return (handle);
 }
--- datadiver/datadiver.h
+++ datadiver/datadiver.h
@@ -1,36 +1,36 @@
-/*
-	datadriver.h
+/**
+ datadriver.h
 
-	Datadiver library
+ Datadiver library
 
-    Copyright (C) Bret Curtis 2014
+ Copyright (C) Bret Curtis 2014
 
-    This file is part of Datadiver.
+ This file is part of Datadiver.
 
-    Datadiver is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+ Datadiver is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-    Datadiver is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+ Datadiver is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef DATADIVER_LIB_H
 #define DATADIVER_LIB_H
 
 /* set our symbol export visiblity */
 #if defined _WIN32 || defined __CYGWIN__
-  /* ========== NOTE TO WINDOWS DEVELOPERS:
-   * If you are compiling for Windows and will link to the static library
-   * (libWildMidi.a with MinGW, or wildmidi_static.lib with MSVC, etc),
-   * you must define DATADIVER_STATIC in your project. Otherwise dllimport
-   * will be assumed. */
+/* ========== NOTE TO WINDOWS DEVELOPERS:
+ * If you are compiling for Windows and will link to the static library
+ * (libWildMidi.a with MinGW, or wildmidi_static.lib with MSVC, etc),
+ * you must define DATADIVER_STATIC in your project. Otherwise dllimport
+ * will be assumed. */
 # if defined(DATADIVER_BUILD) && defined(DLL_EXPORT)		/* building library as a dll for windows */
 #  define DD_SYMBOL __declspec(dllexport)
 # elif defined(DATADIVER_BUILD) || defined(DATADIVER_STATIC)	/* building or using static lib for windows */
@@ -42,9 +42,9 @@
 # if defined(SYM_VISIBILITY)	/* __GNUC__ >= 4, or older gcc with backported feature */
 #  define DD_SYMBOL __attribute__ ((visibility ("default")))
 /*
-# elif defined(__SUNPRO_C) && (__SUNPRO_C >= 0x590)
-#  define DD_SYMBOL __attribute__ ((visibility ("default")))
-*/
+ # elif defined(__SUNPRO_C) && (__SUNPRO_C >= 0x590)
+ #  define DD_SYMBOL __attribute__ ((visibility ("default")))
+ */
 # elif defined(SYM_LDSCOPE)	/* __SUNPRO_C >= 0x550 */
 #  define DD_SYMBOL __global
 # else
@@ -61,38 +61,40 @@ extern "C" {
 #endif
 
 struct _dd_context;
-typedef _dd_context *dd_context;
+typedef _dd_context *DataDiver;
 
-/*!
+/**
  * Retrieves the version of the library.
  *
- * \return long (our version information in long format)
+ * @return long (our version information in long format)
  */
-DD_SYMBOL long DataDiver_GetVersion (void);
+DD_SYMBOL long DataDiver_GetVersion(void);
 
-/*!
- * Creates a context that we use through the life of the file or buffer we are
+/**
+ * Creates a handle that we use through the life of the file or buffer we are
  * working with.
  *
- * \return dd_context (a pointer to the location in memory we are working on.)
+ * @return DataDiver a handle to our DataDiver context.
  */
-DD_SYMBOL dd_context DataDiver_CreateContext (void);
+DD_SYMBOL DataDiver DataDiver_CreateContext(void);
 
-/*!
- * Shuts down the "handle" or context and releases the allocated memory.
+/**
+ * Destroys the DataDiver handle, releases the allocated memory and sets it to null.
  *
- * \param context (a pointer to our context handle)
- * \return int (a boolean)
+ * @param handle a DataDiver context.
+ * @return int (a boolean)
  */
-DD_SYMBOL int DataDiver_DestroyContext (dd_context context);
+DD_SYMBOL int DataDiver_DestroyContext(DataDiver handle);
 
-DD_SYMBOL int DataDiver_OpenFile (dd_context context, const char *path);
+DD_SYMBOL int DataDiver_OpenFile(DataDiver handle, const char *path);
 
-DD_SYMBOL int DataDiver_OpenBuffer (dd_context context, uint8_t *buffer, uint32_t size);
+DD_SYMBOL int DataDiver_OpenBuffer(DataDiver handle, uint8_t *buffer,
+		uint32_t size);
 
-DD_SYMBOL int DataDiver_SetOption (dd_context context, uint16_t option, uint16_t value);
+DD_SYMBOL int DataDiver_SetOption(DataDiver handle, uint16_t option,
+		uint16_t value);
 
-DD_SYMBOL int DataDiver_GetOption (dd_context context, uint16_t option);
+DD_SYMBOL int DataDiver_GetOption(DataDiver handle, uint16_t option);
 
 #if defined(__cplusplus)
 }
